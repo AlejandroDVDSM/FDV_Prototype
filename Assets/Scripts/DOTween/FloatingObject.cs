ec2dfffe613c649ce88eea97c207e9d5aa2f3807
AlejandroDVDSM <bysalexx@gmail.com>
2025-01-06 17:43:49 +0000

    Fix DOTween warning

--- Assets/Scripts/DOTween/FloatingObject.cs
+++ Assets/Scripts/DOTween/FloatingObject.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 
@@ -11,5 +12,10 @@ public class FloatingObject : MonoBehaviour
     private void MakeObjectFloat()
     {
         transform.DOMoveY(transform.position.y + .4f, 2).SetEase(Ease.InOutCubic).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    private void OnDestroy()
+    {
+        transform.DOKill();
     }
 }
